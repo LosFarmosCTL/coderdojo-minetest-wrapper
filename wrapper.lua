a44e4e6ce2830a8e04efc5d4a1260f025594ff06
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-06-08 04:51:31 +0200

    feat: add chat/commands wrapper

--- wrapper.lua
+++ wrapper.lua
@@ -1,1 +1,22 @@
 mod = {}
+
+-----------------------------------
+-------------- Chat ---------------
+-----------------------------------
+
+function mod.chat(message)
+  core.chat_send_all(message)
+end
+
+function mod.neuer_command(name, callback)
+  core.register_chatcommand(name, {
+    func = function(_, param)
+      local answer = callback(param)
+      if answer then
+        return true, answer
+      end
+
+      return true
+    end,
+  })
+end
